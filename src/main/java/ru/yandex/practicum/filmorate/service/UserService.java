e157354b741bf7417033538afe069cb3037b2784
Andrey Abramov <andrey_427@mail.ru>
2025-08-09 13:32:20 +0900

    грохнул InMemoryUserStorage

--- src/main/java/ru/yandex/practicum/filmorate/service/UserService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/UserService.java
@@ -1,7 +1,7 @@
 package ru.yandex.practicum.filmorate.service;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 import ru.yandex.practicum.filmorate.exception.ResourceNotFoundException;
 import ru.yandex.practicum.filmorate.model.User;
@@ -13,12 +13,9 @@ import java.util.Optional;
 
 @Service
 @Slf4j
+@RequiredArgsConstructor
 public class UserService {
     private final UserStorage storage;
-
-    public UserService(@Qualifier("USERDBSTORAGE") UserStorage storage) {
-        this.storage = storage;
-    }
 
     public User createUser(final User user) {
         user.setId(getNextId());
